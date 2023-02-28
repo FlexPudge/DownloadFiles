734883ce5d68b3f713b5d1eb7148a49aea71c80d
FlexPudge <micha.parmuzin@yandex.ru>
2023-02-28 21:35:14 +0300

    First step

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using DownloadFiles.Service;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -18,11 +21,34 @@ namespace DownloadFiles
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window,INotifyPropertyChanged
     {
+        private string _urlAddress;
+        public string UrlAddress
+        {
+            get { return _urlAddress; }
+            set { _urlAddress = value; OnPropertyChanged(nameof(UrlAddress)); }
+        }
+        public string HTML { get; set; }
         public MainWindow()
         {
             InitializeComponent();
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string s = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(s));
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (UrlAddress != null)
+            {
+                GetHTML();
+            } 
+        }
+        private void GetHTML()
+        {
+            HTML = new GetRequest(UrlAddress).Response;
         }
     }
 }
---
+++ Service/GetRequest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DownloadFiles.Service
+{
+    public class GetRequest
+    {
+        private HttpWebRequest _request;
+        private string _address;
+        public string Response { get; set; }
+
+        public GetRequest(string address)
+        {
+            _address = address;
+            Run();
+        }
+
+        public void Run()
+        {
+            _request = (HttpWebRequest)WebRequest.Create(_address);
+            _request.Method = "GET";
+            try
+            {
+                HttpWebResponse response = (HttpWebResponse)_request.GetResponse();
+                var stream = response.GetResponseStream();
+                if (stream != null) Response = new StreamReader(stream).ReadToEnd();
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.Message.ToString());
+            }
+        }
+    }
+}
